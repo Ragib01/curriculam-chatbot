15d79a41d25519314bd0a9b67fd7c27d8bf8fdee
unknown <ragibsh.me@gmail.com>
2024-03-04 15:07:56 +0600

    course yml

--- actions/actions.py
+++ actions/actions.py
@@ -21,7 +21,7 @@ class RestaurantAPI(object):
 
     def format_restaurants(self, df) -> str:
         html = "<table style='border-collapse: collapse; border: 1px solid black;'>"
-        html += "<tr><th style='border: 1px solid black;'>কোর্সের নাম</th><th style='border: 1px solid black;'>রেটিং</th><th style='border: 1px solid black;'>প্রদানকারী</th><th style='border: 1px solid black;'>সময়</th></tr>"
+        html += "<tr><th style='border: 1px solid black;'>কোর্সের নাম</th><th style='border: 1px solid black;'>Rating</th><th style='border: 1px solid black;'>প্রদানকারী</th><th style='border: 1px solid black;'>সময়</th></tr>"
         for _, row in df.iterrows():
             html += f"<tr><td style='border: 1px solid black;'>{row['course_name_bn']}</td><td style='border: 1px solid black;'>{row['Rating']}</td><td style='border: 1px solid black;'>{row['provider']}</td><td style='border: 1px solid black;'>{row['time']}</td></tr>"
         html += "</table>"
