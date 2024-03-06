7fbe7bace9855746044cbcc05d0231be1d0da491
unknown <ragibsh.me@gmail.com>
2024-03-06 12:52:44 +0600

    add default fallback

--- actions/actions.py
+++ actions/actions.py
@@ -87,7 +87,7 @@ class ActionRestaurantsDetail(Action):
 
 class ActionFallback(Action):
     def name(self) -> Text:
-        return "action_fallback"
+        return "action_agri_faq"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -97,40 +97,40 @@ class ActionFallback(Action):
         dispatcher.utter_message(text=answer)
         return []
     
-class ActionAgriFaq(Action):
+# class ActionAgriFaq(Action):
 
-    def name(self) -> Text:
-        return "action_agri_faq"
+#     def name(self) -> Text:
+#         return "action_agri_faq"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # print(tracker.latest_message)
+#         # print(tracker.latest_message)
         
-        # to get intent of user message
-        _intent=tracker.latest_message['intent'].get('name')
-        print("Intent of user message predicted by Rasa ",_intent)
+#         # to get intent of user message
+#         _intent=tracker.latest_message['intent'].get('name')
+#         print("Intent of user message predicted by Rasa ",_intent)
 
-        print(tracker.latest_message['text']) # to get user typed message 
+#         print(tracker.latest_message['text']) # to get user typed message 
 
-        intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
+#         intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
         
-        # confidence of retrieval intent we found
-        retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
-        print(f"retrieval_intent_confidence we found was {retrieval_intent_confidence}")
+#         # confidence of retrieval intent we found
+#         retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
+#         print(f"retrieval_intent_confidence we found was {retrieval_intent_confidence}")
 
-        if retrieval_intent_confidence < 100:
-            if "category-1" in _intent:
-                print("Custom Fallback for category-1 can be implemented here")
-                dispatcher.utter_message(text="Custom Fallback for category-1 can be implemented here")
-            elif "category-2" in _intent:
-                print("Custom Fallback for category-2 can be implemented here")
-                dispatcher.utter_message(text="Custom Fallback for category-2 can be implemented here")
-            return []
-        #used eval to remove quotes around the string
-        intent_found = f'utter_{eval(intent_found)}'
-        print('after adding utter we found -- ', intent_found)
-        dispatcher.utter_message(response = intent_found) # use response for defining intent name
+#         if retrieval_intent_confidence < 100:
+#             if "category-1" in _intent:
+#                 print("Custom Fallback for category-1 can be implemented here")
+#                 dispatcher.utter_message(text="Custom Fallback for category-1 can be implemented here")
+#             elif "category-2" in _intent:
+#                 print("Custom Fallback for category-2 can be implemented here")
+#                 dispatcher.utter_message(text="Custom Fallback for category-2 can be implemented here")
+#             return []
+#         #used eval to remove quotes around the string
+#         intent_found = f'utter_{eval(intent_found)}'
+#         print('after adding utter we found -- ', intent_found)
+#         dispatcher.utter_message(response = intent_found) # use response for defining intent name
         
-        return [] # setting slot values
+#         return [] # setting slot values
